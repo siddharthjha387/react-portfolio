17271565cb03e0c05c2e32178a618e7f1dd63e03
siddharthjha387 <siddharthjha3901@gmail.com>
2022-12-09 09:48:01 +0530

    added fetchoptions

--- src/index.js
+++ src/index.js
@@ -5,15 +5,14 @@ import App from './App';
 import { ThemeProvider } from './context';
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
+  cache: new InMemoryCache(),
     uri: "https://leetcode.com/graphql",
-    // "Access-Control-Allow-Credentials" : true,
-    fetchOptions: {
-      mode: 'no-cors',
-      // mode: 'cors',
-      // headers: headers
-  },
+   
+    fetchOptions:{
+      mode:'no-cors'
+    },
   
-    cache: new InMemoryCache()
+    
   });
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
