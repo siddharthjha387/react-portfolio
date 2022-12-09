1b736725d826626d17a13448846d9b4ffc521d0f
siddharthjha387 <siddharthjha3901@gmail.com>
2022-12-09 11:42:33 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -5,22 +5,8 @@ import App from './App';
 import { ThemeProvider } from './context';
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
-  
     uri: "/graphql",
-    cache: new InMemoryCache()
-    // fetchOptions:{
-    //   mode:'no-cors',
-      
-    // },
-    // headers: {
-    //   "Access-Control-Allow-Origin": "*",
-    //   "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
-    //   "Access-Control-Allow-Headers":
-    //     "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
-    // },
-    // credentials: 'omit'
-  
-    
+    cache: new InMemoryCache(),
   });
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
